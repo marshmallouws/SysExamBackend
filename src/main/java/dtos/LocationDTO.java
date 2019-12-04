2a3649bdeb7804ad6e86f33707aad224b09081eb
vl48 <wegokr@gmail.com>
2019-12-04 19:10:46 +0100

    added airport IDs for api to locations

--- src/main/java/dtos/LocationDTO.java
+++ src/main/java/dtos/LocationDTO.java
@@ -10,12 +10,21 @@ package dtos;
  * @author Victor
  */
 public class LocationDTO {
-    public String name, address, airport;
+    public String name, address, airport, airport_id;
 
-    public LocationDTO(String name, String address, String airport) {
+    public LocationDTO(String name, String address, String airport, String airport_id) {
         this.name = name;
         this.address = address;
         this.airport = airport;
+        this.airport_id = airport_id;
+    }
+
+    public void setAirport_id(String airport_id) {
+        this.airport_id = airport_id;
+    }
+
+    public String getAirport_id() {
+        return airport_id;
     }
 
     public void setAirport(String airport) {
